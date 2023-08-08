f600a03ce5f033c56051e21cc9c84a3b2407f44b
Dennis Mwangi <138608199+Dennis-datascientist@users.noreply.github.com>
2023-08-08 10:00:04 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -40,7 +40,7 @@ for i in range(60, len(train)):
 x_train, y_train = np.array(x_train), np.array(y_train)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-from keras.models import load_model
+
 model = load_model("saved_lstm_model.h5")
 
 inputs = new_data[len(new_data)-len(valid)-60:].values
