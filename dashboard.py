19d3151e1cd943e6558faeb3d632e6d7ae0e91db
Dennis Mwangi <138608199+Dennis-datascientist@users.noreply.github.com>
2023-08-08 09:36:15 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
-import TensorFlow
+import tensorFlow
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
